ccd1912115799bc5fac9f0a02c985c4ea3a38fc4
Vincent Boisard <boisard.v@gmail.com>
2011-10-06 16:47:21 +0200

    update apn version

--- lib/apn_on_rails/app/models/apn/app.rb
+++ lib/apn_on_rails/app/models/apn/app.rb
@@ -138,10 +138,10 @@ class APN::App < APN::Base
           if read_from
             puts "Device #{device.inspect} did a bad bad thing..."
             puts "read_from: #{read_from.inspect}"
-            # read_buffer = read_from.read(6)
-            # puts "CMD: #{read_buffer[0].ord}"
-            # puts "ERR: #{read_buffer[1].ord}"
-            # puts "DEV: #{read_buffer[2..5]}"
+            read_buffer = read_from[0].read(6)
+            puts "CMD: #{read_buffer[0].ord}"
+            puts "ERR: #{read_buffer[1].ord}"
+            puts "DEV: #{read_buffer[2..5]}"
           else
             puts "Timeout!"
           end
