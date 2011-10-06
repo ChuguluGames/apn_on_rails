b8899cafaea2f6ccdef821dd5145a4becf2238ab
Vincent Boisard <boisard.v@gmail.com>
2011-10-06 15:08:42 +0200

    TRY readchar

--- lib/apn_on_rails/app/models/apn/app.rb
+++ lib/apn_on_rails/app/models/apn/app.rb
@@ -135,11 +135,13 @@ class APN::App < APN::Base
           conn.write(gnoty.message_for_sending(device))
           puts "#{conn.methods.sort}"
           puts "Closed? #{conn.closed?}"
-          result = conn.read(6)
-          puts "result: #{result.size}"
-          puts "#{result[0].ord}"
-          puts "#{result[1].ord}"
-          puts "#{result[2..5]}"
+          # result = conn.read(6)
+          # puts "result: #{result.size}"
+          # puts "#{result[0].ord}"
+          # puts "#{result[1].ord}"
+          # puts "#{result[2..5]}"
+          chr = conn.readchar
+          puts "char readed: #{chr[0].ord"
           puts "#{nb_cur_device += 1}/#{gnoty.devices.size} sended"
           try_number = 0
         end
