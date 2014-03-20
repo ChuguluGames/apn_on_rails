ef14ae305ba8d120de6629ddcca118b3c66f3237
zomfg <sergio.kunats@gmail.com>
2014-03-20 18:05:38 +0100

    notification: better message "packing"

--- lib/apn_on_rails/app/models/apn/notification.rb
+++ lib/apn_on_rails/app/models/apn/notification.rb
@@ -80,8 +80,8 @@ class APN::Notification < APN::Base
   # Creates the binary message needed to send to Apple.
   def message_for_sending
     json = self.to_apple_json.gsub(/\\u([0-9a-z]{4})/) {|s| [$1.to_i(16)].pack("U")} # This will create non encoded string. Otherwise the string is encoded from utf8 to ascii with unicode representation (i.e. \\u05d2)
-    device_token = [self.device.token.gsub(/[<\s>]/, '')].pack('H*')
-    message = [0, 0, 32, device_token, 0, json.bytes.count, json].pack('ccca*cca*')
+    device_token = [self.device.token.gsub(/[<\s>]/, '')].pack('H64') # 32 hex bytes
+    message = [0, 32, device_token, json.bytes.count, json].pack('CSa32Sa*') # CMD(uint8), token len(uint16), token(bin), payload len(uint16), payload(bin)
     raise APN::Errors::ExceededMessageSizeError.new(message) if json.bytes.count > 256
     message
   end
